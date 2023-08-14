7cbd83532b48c887e0a992fbf73cfc7bcfd3af90
Gabriel Rossatto <rossattogabriel@gmail.com>
2023-08-14 19:35:02 -0300

    Finalização do dark mode e light mode

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ themeToggle.addEventListener('change', () => {
         body.classList.remove('dark-theme');
     }
 })
+
+const themeToggles = document.getElementById('change-theme');
+const bodyy = document.body;
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
+
+themeToggle.addEventListener('change', () => {
+  if (themeToggle.checked) {
+    body.classList.add('dark-theme');
+    body.classList.remove('light-theme');
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'inline';
+  } else {
+    body.classList.add('light-theme');
+    body.classList.remove('dark-theme');
+    sunIcon.style.display = 'inline';
+    moonIcon.style.display = 'none';
+  }
+});
+
+
+
+
+
+
+  
+  
+  
